f40acfa03fa2f30098e5a6d1405b89bdf7a9d159
frankzyj2000 <48901610+frankzyj2000@users.noreply.github.com>
2019-03-25 04:03:21 -0400

    Update UnitTest1.cs
    Adding new test cases

--- CalculatorTest/UnitTest1.cs
+++ CalculatorTest/UnitTest1.cs
@@ -13,10 +13,38 @@ namespace CalculatorTest
         }
 
         [Fact]
+        public void ShouldSubstractTwoNumbers()
+        {
+            var x = new Calculator().Subtract(4, 5);
+            Assert.Equal(x, -1);
+        }
+        
+        [Fact]
         public void ShouldMultiplyTwoNumbers()
         {
             var x = new Calculator().Multiply(3, 2);
             Assert.Equal(6, x);
         }
+        
+        [Fact]
+        public void ShouldDivideTwoNumbers()
+        {
+            var x = new Calculator().Divide(4, 4);
+            Assert.Equal(1, x);
+        }
+  
+        [Fact]
+        public void ShouldDivideTwoNumbersWithDecimalResult()
+        {
+            var x = new Calculator().Divide(5, 2);
+            Assert.Equal(2.5, x);
+        }
+        
+        [Fact]
+        public void ShouldNotDivideOneNumberByZero()
+        {
+            var x = new Calculator().Divide(4, 0);
+            Assert.Equal(null, x);
+        }    
     }
 }
